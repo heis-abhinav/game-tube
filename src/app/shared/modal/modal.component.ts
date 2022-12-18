66012bfa2c7864e5270e9c39266a44910927fe03
Abhinav <abhinavkumr77@gmail.com>
2022-12-18 14:00:40 +0530

    distinguishing modals by id

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,18 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  styleUrls: ['./modal.component.css'],
+  // providers: [ModalService]
 })
 export class ModalComponent implements OnInit {
 
-  constructor(modal: ModalService) {
-    console.log(modal.visible)
+  @Input() modalID = ''
+  constructor(public modal: ModalService) {
    }
 
   ngOnInit(): void {
+  }
+  closeModal() {
+    this.modal.toggleModal(this.modalID)
   }
 
 }
